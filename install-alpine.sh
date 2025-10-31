04e7731d59b4a3e11cafea9ecb192b387673b513
JustLikeCheese <854675824@qq.com>
2025-10-31 14:08:31 +0800

    feat: upload install-alpine.sh

---
+++ install-alpine.sh
@@ -0,0 +1,20 @@
+echo "Installing alpine packages..."
+apk --no-cache add ca-certificates wget git
+wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
+apk add glibc-2.35-r1.apk glibc-bin-2.35-r1.apk glibc-dev-2.35-r1.apk glibc-i18n-2.35-r1.apk
+
+apk add font-adobe-100dpi
+
+echo "Installing Msyh fonts..."
+cp assets/fonts/msyh.ttc /usr/share/fonts/msyh
+cp assets/fonts/msyhbd.ttc /usr/share/fonts/msyh
+cp assets/fonts/msyhl.ttc /usr/share/fonts/msyh
+
+chmod 777 /usr/share/fonts/msyh/msyh.ttc
+chmod 777 /usr/share/fonts/msyh/msyhbd.ttc
+chmod 777 /usr/share/fonts/msyh/msyhl.ttc
+
+fc-cache -f
+fc-list
+
+echo "Installed successfully. 你好, 世界!"
